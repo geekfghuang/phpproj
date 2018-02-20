a260d852a15e6bca3e97be4ae1e06bba52d8aa33
geekfghuang <geek_huangfugui@163.com>
2018-02-20 18:24:29 +0800

    CI blog

--- lara/app/Http/routes.php
+++
@@ -1,9 +0,0 @@
-<?php  //路由文件
-
-	$app['router']->get('/',function(){
-		return '<h1>路由成功哈哈！！！</h1>';
-	});
-
-	$app['router']->get('/welcome','App\Http\Controllers\WelcomeController@index');
-	
-?>
